e6b4af827149c0244b7b0005c90825fdb6a4a582
Adrian Rajkamal <adrianrajkamal@outlook.com>
2023-06-25 13:59:02 +1000

    Relative imports fixed

--- Experiments/VAE.py
+++ Experiments/VAE.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Handle import issues by referencing parent directory via absolute paths
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from workflow import *
 from networks import *
 
